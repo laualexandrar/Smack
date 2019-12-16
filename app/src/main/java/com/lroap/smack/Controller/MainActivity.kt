2c4ef5dc3cd4d9ad116963937fd65532c3dc8b3d
lauraalexandraroa <laualexandrar@gmail.com>
2019-12-16 14:35:22 -0800

    lesson 07

--- app/src/main/java/com/lroap/smack/Controller/MainActivity.kt
+++ app/src/main/java/com/lroap/smack/Controller/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.lroap.smack.Controller
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 
@@ -8,8 +12,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.lroap.smack.R
+import com.lroap.smack.Services.AuthService
+import com.lroap.smack.Services.UserDataService
+import com.lroap.smack.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,16 +27,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(toolbar) // revisar esta parte la linea de arriba no va en el codigo original que es toolbar
 
         val toggle = ActionBarDrawerToggle(
-            this, drawer_layout, toolbar,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
+            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
         )
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
+        LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver,
+            IntentFilter(BROADCAST_USER_DATA_CHANGE))
+
+    }
+
+    private val userDataChangeReceiver = object: BroadcastReceiver(){
+        override fun onReceive(context: Context?, intent: Intent?) {
+             if (AuthService.isLoggedIn){
+                userNameNavHeader.text = UserDataService.name
+                 userEmailNavHeader.text = UserDataService.email
+                 val resourceId = resources.getIdentifier(UserDataService.avatarName, "drawable",
+                     packageName)
+                 userImageNavHeader.setImageResource(resourceId)
+                 userImageNavHeader.setBackgroundColor(UserDataService.returnAvatarColor(UserDataService.avatarColor))
+                 loginBtnNavHeader.text =  "Logout"
+
+            }
+        }
     }
 
     override fun onBackPressed() {
@@ -42,8 +67,20 @@ class MainActivity : AppCompatActivity() {
 
     fun loginBtnNavClicked (view:View){
 
-        val loginIntent = Intent(this, LoginActivity::class.java) // aqui creo el intent
-        startActivity(loginIntent) // con esto logro que desde el loginBtnNavClicked al oprimirlo yo vaya a la siguiente actividad  LoginActivity que contiene activity_login
+        if(AuthService.isLoggedIn){
+            //logout
+            UserDataService.logout()
+            userNameNavHeader.text = "Login"
+            userEmailNavHeader.text = ""
+            userImageNavHeader.setImageResource((R.drawable.profiledefault))
+            userImageNavHeader.setBackgroundColor(Color.TRANSPARENT)
+            loginBtnNavHeader.text = "Login"
+
+        } else{
+            val loginIntent = Intent(this, LoginActivity::class.java) // aqui creo el intent
+            startActivity(loginIntent) // con esto logro que desde el loginBtnNavClicked al oprimirlo yo vaya a la siguiente actividad  LoginActivity que contiene activity_login
+        }
+
     }
 
     fun addChannelClicked (view: View){
--- app/src/main/java/com/lroap/smack/Utilities/Constants.kt
+++ app/src/main/java/com/lroap/smack/Utilities/Constants.kt
@@ -2,4 +2,8 @@ package com.lroap.smack.Utilities
 
 const val  BASE_URL = "https://chattychatlau.herokuapp.com/v1/"
 const val  URL_REGISTER = "${BASE_URL}account/register"
+const val URL_LOGIN = "${BASE_URL}account/login"
+const val URL_CREATE_USER = "${BASE_URL}user/add"
 
+//broadcast Constants
+const val BROADCAST_USER_DATA_CHANGE = "BROADCAST_USER_DATA_CHANGE"
